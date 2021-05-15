446cbdf0ada085b9bc944d74addeb4f7a8b766ea
KhadidjaHbl <57768834+KhadidjaHbl@users.noreply.github.com>
2021-05-15 20:01:10 +0100

    Update index.ts

--- listvehiclesapi/src/routes/index.ts
+++ listvehiclesapi/src/routes/index.ts
@@ -6,8 +6,6 @@ const router = Router();
 
 router.get('/', get);
 router.get('/vehicle', getV);
-router.get('/Allocated', getAllocatedVehicles);
-router.get('/Availible', getAvailibleVehicles);
 //router.post('/vehicle', addVehicle);
 router.post('/state', addState);
 router.delete('/vehicle/:id', deleteVehicle);
