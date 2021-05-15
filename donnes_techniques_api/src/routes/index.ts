d0c8cc189b07d276fc850c0224f5696521ef629a
Saadia <hs_boushaba@esi.dz>
2021-05-15 13:01:27 +0100

    suppression du dossier .vscode

--- donnes_techniques_api/src/routes/index.ts
+++ donnes_techniques_api/src/routes/index.ts
@@ -6,11 +6,12 @@ const router = Router();
 //user
 router.get('/', get);
 //etat de vehicule 
+//get all list of vehicle State 
 router.get('/vehicle_State', getVehicleState);
 router.post('/vehicle_State', createVehicleState);
 router.put('/vehicle_State/:id_state', updateVehicleState);
 router.delete('/vehicle_State/:id_state', deleteVehicleState);
-//get vehicle state by idRental 
+//get vehicle state by idVehicle 
 router.get('/vehicle_State/:idVehicle', findVehicleState);
 //get rebtal and tenant information by id vehicle 
 router.get('/suivi_vehicule/:idVehicle', findVehicleRental);
