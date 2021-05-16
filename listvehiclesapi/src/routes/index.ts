083ccc19f0d8338fac42a087c64c6f21693122a4
safirihani <gs_rihani@esi.dz>
2021-05-16 16:05:50 +0100

    fix: remove conflicts in imports

--- listvehiclesapi/src/routes/index.ts
+++ listvehiclesapi/src/routes/index.ts
@@ -1,9 +1,5 @@
 import { Router } from 'express';
-<<<<<<< HEAD
 import { get,getVehicles } from '../controllers/listVehicles';
-=======
-import { get, getV,deleteVehicle,getVehicles } from '../controllers/listVehicles';
->>>>>>> f7eb2d5985bff8962cfaa6d70c18baf48176386f
 
 const router = Router();
 
