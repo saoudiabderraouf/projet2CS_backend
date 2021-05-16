9a93d079e4d7d8db0d66321940622ce39c180011
safirihani <gs_rihani@esi.dz>
2021-05-16 15:59:53 +0100

    merge with main file

--- listvehiclesapi/src/routes/index.ts
+++ listvehiclesapi/src/routes/index.ts
@@ -9,11 +9,6 @@ const router = Router();
 
 
 router.get('/', get);
-<<<<<<< HEAD
 router.get('/vehicle', getVehicles);
-=======
-router.get('/vehicle', getV);
-router.delete('/vehicle/:id', deleteVehicle);
->>>>>>> f7eb2d5985bff8962cfaa6d70c18baf48176386f
 
 export default router;
