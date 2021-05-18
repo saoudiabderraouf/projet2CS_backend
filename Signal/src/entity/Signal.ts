036f2a3578dae6e6db3d63c31b428bb49a4d39d9
Meziane Dahou <hd_meziane@esi.dz>
2021-05-18 03:39:02 +0100

    signal api added

---
+++ Signal/src/entity/Signal.ts
@@ -0,0 +1,27 @@
+import {Entity, PrimaryGeneratedColumn, Column, BaseEntity, Timestamp} from "typeorm";
+
+@Entity("Signal")
+export class Signal extends BaseEntity {
+
+    @PrimaryGeneratedColumn()
+    idSignal: number;
+
+    @Column()
+    signalType: string
+
+    @Column()
+    message: string;
+
+    @Column()
+    sourceType: string;
+
+    @Column()
+    idUserSource: number
+
+    @Column()
+    idVehicle: number
+
+    @Column()
+    sent_at: number
+   
+}
