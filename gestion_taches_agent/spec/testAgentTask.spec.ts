8d6b8b48bb82e1a80c7a77e2e1547c8af72c767d
BitsHeart <ga_bennecer@esi.dz>
2021-05-18 03:39:53 +0100

    test: Add / endpoint test 🧪

--- gestion_taches_agent/spec/testAgentTask.spec.ts
+++ gestion_taches_agent/spec/testAgentTask.spec.ts
@@ -1,143 +1,28 @@
-import { Request, json } from "express";
-import { Server } from "http";
-import * as express from 'express';
+const request = require("supertest");
 
 interface IData {
-    status?: number;
-    body?: string;
+  status?: number;
+  body?: string | any;
 }
-const app = express()
 
-let Request = require("request");
-    
 describe("Service Test : ", () => {
-    // let service: Server;
-    // beforeAll(() => {
-    //     service = require("../src/index");
-    //     console.log("sssssssssssssssssss", service); 
-    // });
-    // afterAll(() => {
-    //     service.close();
-    // }); /*  */
-
-    describe("GET /", () => {
+  describe("Endpoint : /", () => {
     let data: IData = {};
-    beforeAll((done) => {
-        Request.get(
-            "http://localhost:8000",(_error: any, response: any, body: any) => {
-              data.status = response.statusCode;
-              data.body = body;
-              console.log("GET / http://localhost:8000 : data.status = ", data.status);
-              done();
-            }
-        );
-        
-    });
-    it("Status 200", () => {
-        expect(data.status).toBe(200);
+
+    it("Returns welcome body successfully", (done) => {
+      request("http://localhost:8000")
+        .get("/")
+        .expect(200)
+        .end((err: Error, res: Response) => {
+          if (err) {
+            console.log(err);
+          }
+          const resultData = res.text;
+          const expectedData =
+            "Hello, this is the agent Tasks' management service.";
+          expect(resultData).toEqual(expectedData);
+          done();
         });
     });
-
-    describe("GET /task", () => {
-      let data: IData = {};
-      beforeAll((done) => {
-          Request.get(
-              "http://localhost:8000/task",(_error: any, response: any, body: any) => {
-                data.status = response.statusCode;
-                data.body = body;
-                console.log("GET / http://localhost:8000/task : data.status = ", data.status);
-                done();
-              }
-          );
-      });
-      it("Status 200", () => {
-          expect(data.status).toBe(200);
-          });
-      });
-
-      describe("POST /task", () => {
-        let data: IData = {};
-        beforeAll((done) => {
-            Request.get(
-                "http://localhost:8000/task",(_error: any, response: any, body: any) => {
-                  data.status = response.statusCode;
-                  data.body = body;
-                  console.log("POST / http://localhost:8000/task : data.status = ", data.status);
-                  done();
-                }
-            );
-        });
-        it("Status 200", () => {
-            expect(data.status).toBe(200);
-            });
-        });
-
-        describe("GET /task/5", () => {
-          let data: IData = {};
-          beforeAll((done) => {
-              Request.get(
-                  "http://localhost:8000/task/5",(_error: any, response: any, body: any) => {
-                    data.status = response.statusCode;
-                    data.body = body;
-                    console.log("GET / http://localhost:8000/task/:id : data.status = ", data.status);
-                    done();
-                  }
-              );
-          });
-          it("Status 200", () => {
-              expect(data.status).toBe(200);
-              });
-          });
-
-          describe("PUT /task/5", () => {
-            let data: IData = {};
-            beforeAll((done) => {
-                Request.get(
-                    "http://localhost:8000/task/5",(_error: any, response: any, body: any) => {
-                      data.status = response.statusCode;
-                      data.body = body;
-                      console.log("PUT / http://localhost:8000/task/:id : data.status = ", data.status);
-                      done();
-                    }
-                );
-            });
-            it("Status 200", () => {
-                expect(data.status).toBe(200);
-                });
-            });
-
-            describe("DELETE /task/5", () => {
-              let data: IData = {};
-              beforeAll((done) => {
-                  Request.get(
-                      "http://localhost:8000/task/5",(_error: any, response: any, body: any) => {
-                        data.status = response.statusCode;
-                        data.body = body;
-                        console.log("DELETE / http://localhost:8000/task/:id : data.status = ", data.status);
-                        done();
-                      }
-                  );
-              });
-              it("Status 200", () => {
-                  expect(data.status).toBe(200);
-                  });
-              });
-
-
-              describe("GET /task", () => {
-                let data: IData = {};
-                beforeAll((done) => {
-                    Request.get(
-                        "http://localhost:8000/task?id=1",(_error: any, response: any, body: any) => {
-                          data.status = response.statusCode;
-                          data.body = body;
-                          console.log("GET / http://localhost:8000/task?id=1 : data.status = ", data.status);
-                          done();
-                        }
-                    );
-                });
-                it("Status 200", () => {
-                    expect(data.status).toBe(200);
-                    });
-                });
+  });
 });
