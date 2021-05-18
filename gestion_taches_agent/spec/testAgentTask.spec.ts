1f99757c29f784a569fe45618f468f90eeed45be
BitsHeart <ga_bennecer@esi.dz>
2021-05-18 03:41:28 +0100

    test: add missing tests ( /task endpoint )

--- gestion_taches_agent/spec/testAgentTask.spec.ts
+++ gestion_taches_agent/spec/testAgentTask.spec.ts
@@ -25,4 +25,44 @@ describe("Service Test : ", () => {
         });
     });
   });
+
+  describe("Endpoint : /task", () => {
+    let data: IData = {};
+
+    it("Returns all tasks successfully", (done) => {
+      request("http://localhost:8000")
+        .get("/task")
+        .expect(200)
+        .end((err: Error, res: any) => {
+          if (err) {
+            console.log(err);
+          }
+          const resultData = JSON.parse(res.text);
+          expect(resultData.length).toBeGreaterThan(0);
+          done();
+        });
+    });
+
+    it("Returns task id=8 successfully", (done) => {
+      request("http://localhost:8000")
+        .get("/task/8")
+        .expect(200)
+        .end((err: Error, res: any) => {
+          if (err) {
+            console.log(err);
+          }
+          const resultData = JSON.parse(res.text);
+          const expectedData = {
+            idTask: 8,
+            idAgent: 2,
+            idVehicle: 1,
+            description: "maintenance task :))",
+            idTaskState: 1,
+            idEquipment: 1,
+          };
+          expect(resultData).toEqual(expectedData);
+          done();
+        });
+    });
+  });
 });
