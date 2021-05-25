97910e59c3366ac2298a308bdb4d609401f190c4
Ouarab <hy_ouarab@esi.dz>
2021-05-25 04:09:25 +0100

    test: added vehicleLatestPosition test

--- geolocalisation/spec/test.spec.ts
+++ geolocalisation/spec/test.spec.ts
@@ -1,53 +1,42 @@
 import "jasmine";
 import app, { server } from '../src/index';
 import request = require('supertest');
+import { VehicleTracking } from "../src/entity/VehicleTracking";
+import { VehiclePosition } from "../src/entity/VehiclePosition";
 
-describe('Get all users request', () => {
-    /*afterAll(() => {
-        server.close();  //for the moment this doesn't work idk why but the server closes on its own anyway so...
-    });*/
+describe('Geolocation service', () => {
 
     describe("GET /", function() {
-        it("returns ???", async () => {
+        it("returns status 200 and message", async () => {
             const { status, text } = await request(app).get("/");
-            const expectedResponseBody = "geolocation service is up and running!";
-            const expectedResponseStatusCode = 200;
-            expect(status).toEqual(expectedResponseStatusCode);
-            expect(text).toEqual(expectedResponseBody);
+            const expectedStatus = 200;
+            const expectedText = "geolocation service is up and running!";
+            expect(status).toEqual(expectedStatus);
+            expect(text).toEqual(expectedText);
         });
     });
+
+    describe("GET /vehicleLatestPosition", function() {
+        it("returns status 200 and vehicle latest position", async () => {
+            //mocks des valeurs de retour des fonctions de typeorm
+            spyOn(VehiclePosition, "findOne")
+                .and
+                .returnValue(new Promise<any>((resolve, _reject) => resolve({
+                    idPosition: 3,
+                    idRental: 1
+                })));
+            spyOn(VehicleTracking, "findOne")
+                .and
+                .returnValue(new Promise<any>((resolve, _reject) => resolve({
+                    longitude: 3.0588,
+                    latitude: 36.7538
+                })));
+            const { status, text } = await request(app).get("/vehicleLatestPosition?idRental=1");
+            const expectedStatus = 200;
+            const expectedText = '{ "ok": true, "position": [3.0588, 36.7538] }';
+            expect(status).toEqual(expectedStatus);
+            expect(JSON.parse(text)).toEqual(JSON.parse(expectedText));
+        });
+    });
+
 });
-
-
-
-
-//in-memory database to test for later, maybe mock request will call this db instead
-
-/*
-import { newDb } from 'pg-mem';
-
-let db = newDb();
-
-const query = 
-`CREATE TABLE VehiclePosition
-(
-    idPosition integer,
-    idRental integer
-);
-INSERT INTO VehiclePosition
-	VALUES (1, 1);
-CREATE TABLE VehicleTracking
-(
-    idTrack integer,
-    latitude double precision,
-    longitude double precision,
-    created_at timestamp,
-    idPosition integer
-);
-INSERT INTO VehicleTracking
-    VALUES (1, -122.483696, 37.833818, now(), 1);
-INSERT INTO VehicleTracking
-    VALUES (2, -122.483589, 37.833496, now(), 1);`;
-
-db.public.none(query);
-*/
